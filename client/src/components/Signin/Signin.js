ad31371b05bfe93e090306dd6e3abbb73c19b4c9
ttejuosho <theycallmeflowz@yahoo.com>
2018-07-09 23:02:36 -0500

    more components

--- client/src/components/Signin/Signin.js
+++ client/src/components/Signin/Signin.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import "./Signin.css";
 
 
@@ -6,21 +7,21 @@ import "./Signin.css";
 class Signin extends Component {
     render() {
         return (
-            
-            <div class="container">
-                <div class="row">
-                    <form class="col s12" id="signin" method="post" action="signin">
-                    <div class="row">
-                        <div class="input-field col s6 offset-s3">
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input id="email" name="email" type="email" class="validate" />
+
+            <div className="container">
+                <div className="row">
+                    <form className="col s12" id="signin" method="post" action="signin">
+                    <div className="row">
+                        <div className="input-field col s6 offset-s3">
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input id="email" name="email" type="email" className="validate" />
                                     <label for="email">Email</label>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input id="password" name="password" type="password" class="validate" />
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input id="password" name="password" type="password" className="validate" />
                                     <label for="password">Password</label>
                                 </div>
                             </div>
@@ -28,14 +29,16 @@ class Signin extends Component {
                     </div>
                     </form>
                 </div>
-                <div class="row center">
-                    <button class="btn waves-effect waves-light purple widebtn" type="submit" name="action">
-                            <a class="btntxt" value="Sign In" href="/posts">Log In<i class="mdi-content-send right"></i></a>
+                <div className="row center">
+                    <button className="btn waves-effect waves-light purple widebtn" type="submit" name="action">
+                            <a className="btntxt" value="Sign In" href="/posts">Log In<i className="mdi-content-send right"></i></a>
                     </button>
-                    <p>Not a member yet ? <a class="hplink" href="/signup">Sign Up Here</a></p>
+                    <p>Not a member yet ? <a className="hplink" href="/signup">Sign Up Here</a></p>
                 </div>
             </div>
 
         )
     }
 }
+
+export default withRouter (Signin);
--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import "./Signup.css";
 
 
@@ -7,24 +8,24 @@ class Signup extends Component {
 
     render() {
         return (
-            <div class="container">
-                <div class="row">
-                    <form id="signup" name="signup" method="post" action="/signup" class="col s12">
-                        <div class="input-field col s6 offset-s3">
-                            <div class="row">
-                                <div class="input-field col s12">
+            <div className="container">
+                <div className="row">
+                    <form id="signup" name="signup" method="post" action="/signup" className="col s12">
+                        <div className="input-field col s6 offset-s3">
+                            <div className="row">
+                                <div className="input-field col s12">
                                     <input name="name" type="text" />
                                     <label for="name">Name</label>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="input-field col s12">
+                            <div className="row">
+                                <div className="input-field col s12">
                                     <input name="email" type="email" />
                                     <label for="email">Email</label>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="input-field col s12">
+                            <div className="row">
+                                <div className="input-field col s12">
                                     <input name="password" type="password" />
                                     <label for="password">Password</label>
                                 </div>
@@ -32,14 +33,16 @@ class Signup extends Component {
                         </div>
                     </form>
                 </div>
-                <div class="row center">
-                    <button class="btn waves-effect waves-light purple widebtn" type="submit" value="Sign Up" name="action">
-                        <a class="btntxt">Sign Up<i class="mdi-content-send right"></i></a>
+                <div className="row center">
+                    <button className="btn waves-effect waves-light purple widebtn" type="submit" value="Sign Up" name="action">
+                        <a className="btntxt">Sign Up<i className="mdi-content-send right"></i></a>
                     </button>
-                    <p>Already a member ? <a class="hplink" href="/signin">Log in here</a></p>
+                    <p>Already a member ? <a className="hplink" href="/signin">Log in here</a></p>
                 </div>
             </div>
-            
+
         )
     }
 }
+
+export default withRouter(Signup);

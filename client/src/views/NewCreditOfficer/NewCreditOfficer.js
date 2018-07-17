ed1ab4665fd5c71e1fb90f9abfefa4607a190c15
ttejuosho <theycallmeflowz@yahoo.com>
2018-07-16 19:47:26 -0500

    new cx & CO

---
+++ client/src/views/NewCreditOfficer/NewCreditOfficer.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import "./NewCreditOfficer.css";
+
+
+class NewCreditOfficer extends Component{
+    render(){
+        return(
+            <main>
+            <div className="row">
+                <form id="signup" name="signup" method="post" action="/signup" className="col s12">
+                    <div className="input-field col s6 offset-s3">
+                    <h5>Add a new customer</h5>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input name="co-name" type="text" />
+                                <label htmlFor="co-name">Name</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input name="co-address" type="text" />
+                                <label htmlFor="co-address">Address</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input name="co-email" type="email" />
+                                <label htmlFor="co-email">Email</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input name="co-phone" type="text" />
+                                <label htmlFor="co-phone">Phone Number</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input name="branch" type="text" />
+                                <label htmlFor="branch">Branch</label>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div className="row center">
+                <button className="btn waves-effect waves-light" type="submit" value="Sign Up">Add Credit Officer</button>
+            </div>
+        </main>
+        )
+    }
+}
+
+export default withRouter(NewCreditOfficer);
---
+++ client/src/views/NewCustomer/NewCustomer.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import "./NewCustomer.css";
+
+
+class NewCustomer extends Component {
+    render(){
+        return(
+            <main>
+                <div className="row">
+                    <form id="signup" name="signup" method="post" action="/signup" className="col s12">
+                        <div className="input-field col s6 offset-s3">
+                        <h5 className="page-title">Add a new customer</h5>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input name="name" type="text" />
+                                    <label htmlFor="name">Name</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input name="homeAddress" type="text" />
+                                    <label htmlFor="homeAddress">Home Address</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input name="email" type="email" />
+                                    <label htmlFor="email">Email</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input name="phoneNumber" type="text" />
+                                    <label htmlFor="phoneNumber">Phone Number</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input name="businessName" type="text" />
+                                    <label htmlFor="businessName">Business Name</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input name="businessAddress" type="text" />
+                                    <label htmlFor="businessAddress">Business Address</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input name="businessPhone" type="text" />
+                                    <label htmlFor="businessPhone">Business Phone Number</label>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div className="row center">
+                    <button className="btn waves-effect waves-light" type="submit" value="Sign Up">Add Customer</button>
+                </div>
+            </main>
+        )
+    }
+}
+
+export default withRouter(NewCustomer);

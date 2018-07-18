c458a1fd76b81d763ee56657de2af2435124023f
ttejuosho <theycallmeflowz@yahoo.com>
2018-07-18 14:22:50 -0500

    config out

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,17 +3,13 @@ import axios from "axios";
 export default {
     
 // GETS USER INFO
-getUser: function (column, value) {
-    return axios.get(`/api/users/${column}/${value}`);
+getUser: function (id) {
+    return axios.get('/api/users/' + id);
   },
 
   //Create User in User table and Create User in fav table
 createUser: function (newUser) {
     return axios.post("/api/users", newUser)
-        .then(response => {
-        const UserId = { UserId: response.data.id };
-        });
-    }
-
+}
 
 }
